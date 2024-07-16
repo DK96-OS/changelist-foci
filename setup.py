d55b90ed82d9562ced29fdeb1070d220e9a00646
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-07-16 17:05:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="changelist-foci",
-    version="0.1.1",
+    version="0.2",
 	description='Changelist FOCI',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
