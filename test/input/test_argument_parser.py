88ca545127efc6561d6261ffeccc89d3fa8b02d5
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-07-17 18:02:14 -0400

    Test Input Package: *u test_argument_parser.py - add test cases, add assertions for FormatOptions

--- test/input/test_argument_parser.py
+++ test/input/test_argument_parser.py
@@ -7,10 +7,60 @@ def test_parse_arguments_empty_list():
     result = parse_arguments('')
     assert result.changelist_name == None
     assert result.workspace_path is None
+    assert not result.full_path
+    assert not result.no_file_ext
+    assert not result.file_name
 
 
 def test_parse_arguments_change_list_main():
     result = parse_arguments(['--changelist', 'Main'])
     assert result.changelist_name == 'Main'
     assert result.workspace_path is None
+    assert not result.full_path
+    assert not result.no_file_ext
+    assert not result.file_name
 
+
+def test_parse_arguments_filename_plus_no_file_ext():
+    result = parse_arguments(['-fx'])
+    assert result.changelist_name is None
+    assert result.workspace_path is None
+    assert not result.full_path
+    assert result.no_file_ext
+    assert result.file_name
+
+
+def test_parse_arguments_filename():
+    result = parse_arguments(['-f'])
+    assert result.changelist_name is None
+    assert result.workspace_path is None
+    assert not result.full_path
+    assert not result.no_file_ext
+    assert result.file_name
+
+
+def test_parse_arguments_no_file_ext():
+    result = parse_arguments(['-x'])
+    assert result.changelist_name is None
+    assert result.workspace_path is None
+    assert not result.full_path
+    assert result.no_file_ext
+    assert not result.file_name
+
+
+def test_parse_arguments_full_path():
+    result = parse_arguments(['--full-path'])
+    assert result.changelist_name is None
+    assert result.workspace_path is None
+    assert result.full_path
+    assert not result.no_file_ext
+    assert not result.file_name
+
+
+def test_parse_arguments_changelist_filename():
+    result = parse_arguments(['--changelist', "Main", '-f'])
+    assert result.changelist_name == 'Main'
+    assert result.workspace_path is None
+    assert not result.full_path
+    assert not result.no_file_ext
+    assert result.file_name
