95c112b92f6a0e5da90951c67ec9c69c4d7afd15
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-12-06 11:15:23 -0500

    CL-FOCI Main Package: * Update __main__.py - optimize imports

--- changelist_foci/__main__.py
+++ changelist_foci/__main__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-from sys import argv, path
 
 
 def main():
     from changelist_foci.input import validate_input
     from changelist_foci import get_changelist_foci
+    from sys import argv
     input_data = validate_input(argv[1:])
     output_data = get_changelist_foci(input_data)
     print(output_data)
@@ -12,6 +12,7 @@ def main():
 
 if __name__ == "__main__":
     from pathlib import Path
+    from sys import path
     # Get the directory of the current file (__file__ is the path to the script being executed)
     current_directory = Path(__file__).resolve().parent.parent
     # Add the directory to sys.path
