25f02db0df06df6d1440fcccac42cda5bf16b60a
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-09 16:26:45 +0200

    Test Main Package: * Update test_method_insert_foci_comments - realign import statements

--- test/changelist_foci/test_method_insert_foci_comments.py
+++ test/changelist_foci/test_method_insert_foci_comments.py
@@ -2,9 +2,10 @@
 """
 import sys
 import pytest
-from changelist_data import load_storage
+from changelist_data.storage import load_storage
 
-from changelist_foci import insert_foci_comments, FormatOptions
+from changelist_foci.format_options import FormatOptions
+from changelist_foci import insert_foci_comments
 from test.changelist_foci.conftest import CHANGELIST_DATA_FILE_PATH, CHANGELIST_DATA_SAMPLE_1
 
 
@@ -15,7 +16,7 @@ def test_insert_foci_comments_none_raises_exit():
 
 def test_insert_foci_comments_cl_data_sample_1_default_format(temp_cwd):
     sys.argv = ['cl-foci']
-    
+    #
     CHANGELIST_DATA_FILE_PATH.parent.mkdir(parents=True)
     CHANGELIST_DATA_FILE_PATH.touch()
     CHANGELIST_DATA_FILE_PATH.write_text(CHANGELIST_DATA_SAMPLE_1)
