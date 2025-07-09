a8639fe9a185fd37c0cda5348dac39af3bbbfca3
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-09 22:59:19 +0200

    Test Main Package: * Update test_method_insert_foci_comments - add test cases

--- test/changelist_foci/test_method_insert_foci_comments.py
+++ test/changelist_foci/test_method_insert_foci_comments.py
@@ -5,7 +5,8 @@ from changelist_data.storage import load_storage
 
 from changelist_foci.format_options import FormatOptions
 from changelist_foci import insert_foci_comments
-from test.changelist_foci.conftest import CHANGELIST_DATA_FILE_PATH, CHANGELIST_DATA_SAMPLE_1
+from test.changelist_foci.conftest import CHANGELIST_DATA_FILE_PATH, CHANGELIST_DATA_SAMPLE_1, WORKSPACE_DATA_FILE_PATH, \
+    WORKSPACE_DATA_SAMPLE_1
 
 
 def test_insert_foci_comments_none_raises_exit():
@@ -25,12 +26,36 @@ def test_insert_foci_comments_cl_data_sample_1_default_format(temp_cwd):
         assert len(cl.comment) > len(cl.name)
 
 
-def test_insert_foci_comments_(temp_cwd):
+def test_insert_foci_comments_ws_data_sample_1_default_format(temp_cwd):
+    WORKSPACE_DATA_FILE_PATH.parent.mkdir(parents=True)
+    WORKSPACE_DATA_FILE_PATH.touch()
+    WORKSPACE_DATA_FILE_PATH.write_text(WORKSPACE_DATA_SAMPLE_1)
+    cl_data_storage = load_storage()
+    insert_foci_comments(cl_data_storage, FormatOptions())
+    #
+    result = cl_data_storage.get_changelists()
+    for cl in result:
+        assert len(cl.comment) > len(cl.name)
+
+
+def test_insert_foci_comments_cl_data_sample_1_fax_format(temp_cwd):
     CHANGELIST_DATA_FILE_PATH.parent.mkdir(parents=True)
     CHANGELIST_DATA_FILE_PATH.touch()
     CHANGELIST_DATA_FILE_PATH.write_text(CHANGELIST_DATA_SAMPLE_1)
     cl_data_storage = load_storage()
-    insert_foci_comments(cl_data_storage, FormatOptions())
+    insert_foci_comments(cl_data_storage, FormatOptions(no_file_ext=True, file_name=True))
+    #
+    result = cl_data_storage.get_changelists()
+    for cl in result:
+        assert len(cl.comment) > len(cl.name)
+
+
+def test_insert_foci_comments_ws_data_sample_1_fax_format(temp_cwd):
+    WORKSPACE_DATA_FILE_PATH.parent.mkdir(parents=True)
+    WORKSPACE_DATA_FILE_PATH.touch()
+    WORKSPACE_DATA_FILE_PATH.write_text(WORKSPACE_DATA_SAMPLE_1)
+    cl_data_storage = load_storage()
+    insert_foci_comments(cl_data_storage, FormatOptions(no_file_ext=True, file_name=True))
     #
     result = cl_data_storage.get_changelists()
     for cl in result:
